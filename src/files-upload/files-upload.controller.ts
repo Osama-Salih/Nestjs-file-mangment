cce189d5df6c44a5e626aad34d6e083a14626fb4
Osama Salih <osamasalih.ca@gmail.com>
2024-09-03 11:37:50 +0300

    2-Use built-in file pipe to validate single file

--- src/files-upload/files-upload.controller.ts
+++ src/files-upload/files-upload.controller.ts
@@ -1,24 +1,58 @@
 import {
   Controller,
+  FileTypeValidator,
+  HttpStatus,
+  MaxFileSizeValidator,
+  ParseFilePipe,
   Post,
+  UnprocessableEntityException,
   UploadedFile,
+  UploadedFiles,
   UseInterceptors,
 } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 type File = Express.Multer.File;
 
 @Controller('files-upload')
 export class FilesUploadController {
+  @Post('single')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          // 1) validate file type (extension)
+          new FileTypeValidator({
+            fileType: /png|jpg/,
+          }),
+
+          // 2) validate file size
+          new MaxFileSizeValidator({
+            maxSize: 2 * 1024 * 1024,
+            message: (maxSize) =>
+              `File is too big. Max file size is ${maxSize} bytes`,
+          }),
+        ],
+        errorHttpStatusCode: HttpStatus.UNSUPPORTED_MEDIA_TYPE,
+        exceptionFactory: (error: string) => {
+          console.log('error', error);
+          throw new UnprocessableEntityException(error);
+        },
+      }),
+    )
+    file: File,
+  ) {
+    return file;
+  }
+
   @UseInterceptors(
-    FileInterceptor('file', {
-      limits: {
-        fileSize: 2 * 1024 * 1024, // 2 MB  (1 * 1024 one byte)
-      },
+    FilesInterceptor('files', 3, {
+      limits: { fileSize: 2 * 1024 * 1024 }, // 2 MB  (1 * 1024 one byte)
     }),
   )
-  @Post('single')
-  uploadFile(@UploadedFile() file: File) {
-    return file;
+  @Post('multiple')
+  uploadMultipleFiles(@UploadedFiles() files: File[]) {
+    return files.map((file) => file.originalname);
   }
 }
